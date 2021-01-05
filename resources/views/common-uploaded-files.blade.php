25575f72c3111dc43ef2729a5312a6afb77e55c4
deepika <d.badole@e-tailors.com>
2021-01-05 10:34:20 +0530

    message changes

--- resources/views/common-uploaded-files.blade.php
+++ resources/views/common-uploaded-files.blade.php
@@ -61,5 +61,9 @@
         </div>
     @endforeach
 @else
-    <h4>No Data Found</h4>
+    @if(Request::segment(1) == "account")
+        <h4>Welcome {{Auth::user()->name}}, you have not uploaded audio files yet, please <a class="href-link" href="{{URL::to('/')}}/upload-audio">click here</a> to begin</h4>
+    @else
+        <h4>No Data Found</h4>
+    @endif
 @endif
