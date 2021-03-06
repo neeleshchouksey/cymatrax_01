712ce40bee77322c5b8b1db405ff343970b936bf
deepika <nchouksey@manifestinfotech.com>
2021-03-06 11:21:11 +0530

    ok cancel btn on subscription

--- resources/views/free-subscription.blade.php
+++ resources/views/free-subscription.blade.php
@@ -6,7 +6,8 @@
         <h1 class="myaccount">{{$title}}</h1>
         <div>
             Are you sure? You want to confirm free subscription ?
-            <a href="{{URL::to('/')}}/confirm-subscription">Click Here</a>
+            <button class="wave-btn"  onclick="window.location.href = '{{URL::to('/')}}/confirm-subscription'">OK</button>
+            <button class="wave-btn" onclick="window.location.href = '{{URL::to('/')}}/dashboard'">Cancel</button>
         </div>
     </section>
 @endsection
