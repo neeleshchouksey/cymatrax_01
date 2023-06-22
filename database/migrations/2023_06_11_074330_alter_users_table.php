3c451c2c0f51f091a8b6d331b48949a28353696a
Neelesh Chouksey <neel@Neeleshs-MacBook-Air.local>
2023-06-22 23:41:35 +0530

    migrations

--- database/migrations/2023_06_11_074330_alter_users_table.php
+++ database/migrations/2023_06_11_074330_alter_users_table.php
@@ -13,13 +13,13 @@ class AlterUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
+        /*Schema::table('users', function (Blueprint $table) {
             $table->string('name')->nullable()->change();
             $table->tinyInteger('user')->nullable()->change();
             $table->integer('is_verified')->default(0)->after('email_verified_at');
             $table->integer('google_id')->nullable()->after('is_verified');
             $table->dateTime('email_sent_at')->nullable()->after('google_id');
-        });
+        });*/
     }
 
     /**
